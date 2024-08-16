5bf5230cba6e1abf1cb65de1367c4de32194ce23
George Duffett <gduffett@certinia.com>
2024-08-16 21:39:27 +0100

    feat: add humidity reader

---
+++ sense/src/readers/humidity.py
@@ -0,0 +1,38 @@
+import logging
+import time
+from typing import Optional
+
+import adafruit_dht
+import board
+
+
+logger = logging.getLogger(__name__)
+
+
+def read_humidity() -> Optional[float]:
+    sensor = adafruit_dht.DHT11(board.D4)
+
+    while True:
+        try:
+            logger.info('Reading humidity...')
+            humidity = sensor.humidity
+            sensor.exit()
+            logger.info(f'Humidity: {humidity}')
+            return humidity
+    
+        except RuntimeError as error:
+            # Errors happen fairly often, DHT's are hard to read, log and keep going
+            logger.warning(str(error))
+            time.sleep(2.0)
+            continue
+    
+        except Exception as error:
+            logger.error(error)
+            sensor.exit()
+            break
+    
+        except KeyboardInterrupt:
+            logger.info(f'Interrupted. Exiting...')
+            sensor.exit()
+            break
+
