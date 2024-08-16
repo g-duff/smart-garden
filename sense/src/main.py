6e440442e4f321f350309f47758fa44f5e7791d7
George Duffett <gduffett@certinia.com>
2024-08-16 21:40:54 +0100

    feat: call temperature and humidity readers

--- sense/src/main.py
+++ sense/src/main.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+import logging
+
+from readers.temperature import read_temperature
+from readers.humidity import read_humidity
 
 
 @dataclass
@@ -10,9 +14,12 @@ class SensorData:
 
 
 def read() -> SensorData: 
+    temperature = read_temperature()
+    humidity = read_humidity()
+
     return SensorData(
-            temperature=0,
-            humidity=0,
+            temperature=temperature if temperature is float else 0,
+            humidity=humidity if humidity is float else 0,
             moisture_plant1=0,
             moisture_plant2=0
         )
@@ -23,6 +30,9 @@ def write(data: SensorData):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info('Started')
     data = read()
     write(data)
     
