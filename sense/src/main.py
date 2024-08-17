244684011af732e3d901320463c1c488da55088c
George Duffett <gduffett@certinia.com>
2024-08-17 22:02:25 +0100

    feat(sense): write plant data

--- sense/src/main.py
+++ sense/src/main.py
@@ -6,6 +6,7 @@ from readers.humidity import read_humidity
 from readers.moisture import read_moisture
 
 from writers.location import write_location_data
+from writers.plant import write_plant_data
 
 
 if __name__ == '__main__':
@@ -21,5 +22,12 @@ if __name__ == '__main__':
             humidity=humidity if humidity else 0,
             temperature=temperature if temperature else 0,
     )
-    
+
+    moisture_0_percent, moisture_0_voltage = read_moisture(0)
+    write_plant_data(
+            timestamp=datetime.now().isoformat(),
+            moisture_percent=moisture_0_percent,
+            moisture_voltage=moisture_0_voltage,
+    )
+
     logger.info('Finished')
