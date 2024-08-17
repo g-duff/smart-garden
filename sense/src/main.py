1382df67077169e83f551d1ab9b729962c0370db
George Duffett <gduffett@certinia.com>
2024-08-17 21:16:14 +0100

    feat(sense): call location writer

--- sense/src/main.py
+++ sense/src/main.py
@@ -1,41 +1,25 @@
-from dataclasses import dataclass
+from datetime import datetime
 import logging
 
 from readers.temperature import read_temperature
 from readers.humidity import read_humidity
 from readers.moisture import read_moisture
 
-
-@dataclass
-class SensorData:
-    humidity: float
-    moisture_0: float
-    moisture_1: float
-    temperature: float
-
-
-def read() -> SensorData: 
-    humidity = read_humidity()
-    moisture_0 = read_moisture(0)
-    moisture_1 = read_moisture(1)
-    temperature = read_temperature()
-
-    return SensorData(
-            humidity=humidity if humidity is float else 0,
-            moisture_0=moisture_0,
-            moisture_1=moisture_1,
-            temperature=temperature if temperature is float else 0,
-        )
-
-
-def write(data: SensorData):
-    pass
+from writers.location import write_location_data
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.info('Started')
-    data = read()
-    write(data)
+
+    humidity = read_humidity()
+    temperature = read_temperature()
+
+    write_location_data(
+            timestamp=datetime.now().isoformat(),
+            humidity=humidity if humidity else 0,
+            temperature=temperature if temperature else 0,
+    )
     
+    logger.info('Finished')
