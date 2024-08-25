13369e8c4accafa4c831bbf3de1807192a398032
George Duffett <gduffett@certinia.com>
2024-08-25 21:31:20 +0100

    feat(plot): update urls

--- plot/plot.py
+++ plot/plot.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 host = 'http://pi-0.local:5000'
 
-location_df = pd.read_json(f'{host}/json/location/kitchen', orient='records', convert_dates=True)
-plant_df = pd.read_json(f'{host}/json/plant/basil', orient='records', convert_dates=True)
+location_df = pd.read_json(f'{host}/environment/kitchen/json', orient='records', convert_dates=True)
+plant_df = pd.read_json(f'{host}/plant/basil/json', orient='records', convert_dates=True)
 
 fig, (mst_ax, hmd_ax, tmp_ax) = plt.subplots(nrows=3, sharex=True)
 
